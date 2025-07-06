7d2d13fc8d4ce5a99b51120a43750487fe434f96
Pranjali Sehrawat <pranjalisehrawat@gmail.com>
2025-07-06 23:47:58 +0530

    update

--- MegaBlog/src/appwrite/config.js
+++ MegaBlog/src/appwrite/config.js
@@ -5,7 +5,7 @@ import {Client, ID, Databases, Storage, Query} from "appwrite";
 
 export class Service{
     client =new Client();
-    Databases;
+    databases;
     bucket;
     constructor(){
         this.client
@@ -14,14 +14,72 @@ export class Service{
             this.Databases= new Databases(this.client);
             this.bucket =new Storage(this.client);
     }
-    async createPost({title, slug,content, featuredImage,status, userId}){
+    async createPost({title, slug, content, featuredImage, status, userId}){
         try {
-            
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
         } catch (error) {
             console.log("Appwrite service :: createPost :: error",error);
         }
 
     }
+    async updatePost(slug ,{title, content, featuredImage, status}){
+        try {
+             return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+             )
+
+             
+        } catch (error) {
+            console.log("Appwrite service :: updatePost :: error",error);
+
+        }
+    }
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+            return true;
+        } catch (error) {
+          console.log("Appwrite service :: deletePost :: error",error);  
+           return false; 
+        }
+    }
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+           console.log("Appwrite service :: deletePost :: error",error);  
+            return false
+        }
+    }
+    
+
 }
 
 
